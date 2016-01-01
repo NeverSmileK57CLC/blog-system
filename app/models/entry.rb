86850c43368fb50c4fa8e55086c714b6bf96f635
NeverSmilek57CLC <neversmile12b1@gmail.com>
2016-01-01 18:21:54 +0700

    Add show entries

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,8 @@
 class Entry < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
+  validates :title, presence: true
+  validates :content, presence: true
   default_scope -> { order(:created_at => :desc)}
 
   def comment_feed
