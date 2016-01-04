93ead3bdd614573a437c66ffb4b90e2ec013b330
NeverSmilek57CLC <neversmile12b1@gmail.com>
2016-01-04 14:25:12 +0700

    Add time edit

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,9 @@ class Comment < ActiveRecord::Base
   validates :content, presence: true
   validates :user_id, presence: true
   default_scope -> { order(:created_at => :desc)}
+  before_update :set_time_edit
+
+  def set_time_edit
+    self.time_edit = Time.zone.now
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151231033728) do
+ActiveRecord::Schema.define(version: 20160104065455) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20151231033728) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.datetime "time_edit"
   end
 
   add_index "comments", ["entry_id"], name: "index_comments_on_entry_id"
